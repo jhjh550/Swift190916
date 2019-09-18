fd7ee838b010c1fd2c2de88559ade635d36b0c4f
jh <jhjh550@gmail.com>
2019-09-18 14:23:16 +0900

    xml

--- IosSwift190917/IosSwift190917/T05_XML/XmlVC.swift
+++ IosSwift190917/IosSwift190917/T05_XML/XmlVC.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class XmlVC: UIViewController, XMLParserDelegate {
+    
+    class Pizza{
+        var id: Int = 0
+        var name: String = ""
+        var cost: Int = 0
+        var description: String = ""
+    }
+    
+    var pizza = Pizza()
+    var pizzaList = [Pizza]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +30,39 @@ class XmlVC: UIViewController, XMLParserDelegate {
             let parser = XMLParser(contentsOf: url)
             parser?.delegate = self
             parser?.parse()
+            
+            for pizza in pizzaList{
+                print("\(pizza.id) \(pizza.name) \(pizza.cost) \(pizza.description)")
+            }
         }
     }
     
+    var tagString:String = ""
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        <#code#>
+        
+        tagString = elementName
+        if elementName == "item" {
+            pizza = Pizza()
+            pizzaList.append(pizza)
+        }
+        
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
-        print(string)
+        
+        switch tagString {
+        case "id":
+            pizza.id = Int(string) ?? 0
+        case "name":
+            pizza.name = string
+        case "cost":
+            pizza.cost = Int(string) ?? 0
+        case "description":
+            pizza.description = string
+        default:
+            break
+        }
+        tagString = ""
     }
     
 
